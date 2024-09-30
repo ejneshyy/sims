ab1aa44cab505e868e82f3ed5b436340b0b160dd
richmondrosete19@mail.com <almondrich>
2024-09-30 13:34:43 +0800

    edited senior form

--- admin/pages/add-senior.php
+++ admin/pages/add-senior.php
@@ -40,136 +40,169 @@ if ($result && $result->num_rows > 0) {
               </div>
            </div>
            <section class="content">
-              <div class="container-fluid">
-                 <div class="card card-info">
-                    <div class="card card-outline card-info">
-                      <form action="../insert-profile.php" method="post" onsubmit="return validateForm()">
-                         <div class="card-body">
+    <div class="container-fluid">
+        <div class="card card-info">
+            <div class="card card-outline card-info">
+                <form action="../insert-profile.php" method="post" onsubmit="return validateForm()">
+                    <div class="card-body">
+                        
+                        <!-- Personal Details Section -->
+                        <div class="form-section">
+                            <h6 class="section-title">Personal Details</h6>
                             <div class="row">
-                               <div class="col-md-12">
-                                  <div class="card-header">
-                                     <span class="fa fa-user"> Profile Information</span>
-                                  </div>
-                                  <div class="row">
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="seniorId">Senior ID</label>
-                                           <input type="text" id="seniorId" name="senior_id" class="form-control" placeholder="SNR-123" required>
-                                        </div>
-                                     </div>
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="firstName">First Name</label>
-                                           <input type="text" id="firstName" name="first_name" class="form-control" placeholder="First Name" required>
-                                        </div>
-                                     </div>
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="middleName">Middle Name</label>
-                                           <input type="text" id="middleName" name="middle_name" class="form-control" placeholder="Middle Name">
-                                        </div>
-                                     </div>
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="lastName">Last Name</label>
-                                           <input type="text" id="lastName" name="last_name" class="form-control" placeholder="Last Name" required>
-                                        </div>
-                                     </div>
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="gender">Gender</label>
-                                           <select id="gender" name="gender" class="form-control" required>
-                                              <option value="">Select Gender</option>
-                                              <option value="Male">Male</option>
-                                              <option value="Female">Female</option>
-                                           </select>
-                                        </div>
-                                     </div>
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="birthday">Birthday</label>
-                                           <input type="date" id="birthday" name="birthday" class="form-control" onchange="calculateAge()" required>
-                                        </div>
-                                     </div>
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="age">Age</label>
-                                           <input type="number" id="age" name="age" class="form-control" placeholder="Age" readonly>
-                                        </div>
-                                     </div>
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="birthPlace">Birth Place</label>
-                                           <input type="text" id="birthPlace" name="birth_place" class="form-control" placeholder="Birth Place">
-                                        </div>
-                                     </div>
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="religion">Religion</label>
-                                           <input type="text" id="religion" name="religion" class="form-control" placeholder="Religion">
-                                        </div>
-                                     </div>
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="civilStatus">Civil Status</label>
-                                           <select id="civilStatus" name="civil_status" class="form-control">
-                                              <option value="">Select Civil Status</option>
-                                              <option value="Single">Single</option>
-                                              <option value="Married">Married</option>
-                                              <option value="Widowed">Widowed</option>
-                                              <option value="Divorced">Divorced</option>
-                                           </select>
-                                        </div>
-                                     </div>
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="spouseName">Spouse Name</label>
-                                           <input type="text" id="spouseName" name="spouse_name" class="form-control" placeholder="Spouse Name">
-                                        </div>
-                                     </div>
-                                     <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="spouseContact">Spouse Contact Number</label>
-                                           <input type="text" id="spouseContact" name="spouse_contact_number" class="form-control" placeholder="Spouse Contact Number">
-                                        </div>
-                                     </div>
-                                     <!-- Barangay Display -->
-                                 <!-- Barangay Select Field -->
-                           <!-- Barangay Select Field -->
-                           <div class="col-md-3">
-                                        <div class="form-group">
-                                           <label for="barangay">Barangay</label>
-                                           <select id="barangay" name="barangay_assigned" class="form-control" required>
-                                               <option value="">Select Barangay</option>
-                                               <?php foreach ($barangays as $barangay): ?>
-                                                   <option value="<?php echo htmlspecialchars($barangay); ?>">
-                                                       <?php echo htmlspecialchars($barangay); ?>
-                                                   </option>
-                                               <?php endforeach; ?>
-                                           </select>
-                                        </div>
-                                     </div>
-                                     <div class="col-md-12">
-                                        <div class="form-group">
-                                           <label for="address">Address</label>
-                                           <textarea id="address" name="address" class="form-control" placeholder="Address"></textarea>
-                                        </div>
-                                     </div>
-                                     <!-- Additional form fields can be uncommented if needed -->
-                                  </div>
-                               </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="seniorId">Senior ID</label>
+                                        <input type="text" id="seniorId" name="senior_id" class="form-control form-field" placeholder="SNR-123" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="firstName">First Name</label>
+                                        <input type="text" id="firstName" name="first_name" class="form-control form-field" placeholder="First Name" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="middleName">Middle Name</label>
+                                        <input type="text" id="middleName" name="middle_name" class="form-control form-field" placeholder="Middle Name">
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="lastName">Last Name</label>
+                                        <input type="text" id="lastName" name="last_name" class="form-control form-field" placeholder="Last Name" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="gender">Gender</label>
+                                        <select id="gender" name="gender" class="form-control form-field" required>
+                                            <option value="">Select Gender</option>
+                                            <option value="Male">Male</option>
+                                            <option value="Female">Female</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="birthday">Birthday</label>
+                                        <input type="date" id="birthday" name="birthday" class="form-control form-field" onchange="calculateAge()" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="age">Age</label>
+                                        <input type="number" id="age" name="age" class="form-control form-field" placeholder="Age" readonly>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="birthPlace">Birth Place</label>
+                                        <input type="text" id="birthPlace" name="birth_place" class="form-control form-field" placeholder="Birth Place">
+                                    </div>
+                                </div>
                             </div>
-                         </div>
-                         <div class="card-footer">
-                            <button type="submit" class="btn btn-primary">Save</button>
-                         </div>
-                      </form>
+                        </div>
+                        
+                        <!-- Additional Details Section -->
+                        <div class="form-section">
+                            <h6 class="section-title">Additional Details</h6>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="religion">Religion</label>
+                                        <input type="text" id="religion" name="religion" class="form-control form-field" placeholder="Religion">
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="civilStatus">Civil Status</label>
+                                        <select id="civilStatus" name="civil_status" class="form-control form-field">
+                                            <option value="">Select Civil Status</option>
+                                            <option value="Single">Single</option>
+                                            <option value="Married">Married</option>
+                                            <option value="Widowed">Widowed</option>
+                                            <option value="Divorced">Divorced</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="spouseName">Spouse Name</label>
+                                        <input type="text" id="spouseName" name="spouse_name" class="form-control form-field" placeholder="Spouse Name">
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="spouseContact">Spouse Contact Number</label>
+                                        <input type="text" id="spouseContact" name="spouse_contact_number" class="form-control form-field" placeholder="Spouse Contact Number">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Barangay and Address Section -->
+                        <div class="form-section">
+                            <h6 class="section-title">Barangay and Address</h6>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="barangay">Barangay</label>
+                                        <select id="barangay" name="barangay_assigned" class="form-control form-field" required>
+                                            <option value="">Select Barangay</option>
+                                            <?php foreach ($barangays as $barangay): ?>
+                                                <option value="<?php echo htmlspecialchars($barangay); ?>">
+                                                    <?php echo htmlspecialchars($barangay); ?>
+                                                </option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="address">Address</label>
+                                        <textarea id="address" name="address" class="form-control form-field" placeholder="Address"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
                     </div>
-                 </div>
-              </div>
-           </section>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
+</section>
+
+<style>
+    .form-section {
+        margin-bottom: 20px;
+        border: 1px solid #ccc;
+        padding: 15px;
+        border-radius: 5px;
+        background-color: #f9f9f9;
+    }
+    .section-title {
+        font-weight: bold;
+        margin-bottom: 15px;
+        text-transform: uppercase;
+        font-size: 14px;
+        border-bottom: 1px solid #ddd;
+        padding-bottom: 5px;
+    }
+    .form-field {
+        border: 1px solid #333;
+        box-shadow: none;
+    }
+    .card-footer {
+        text-align: center;
+    }
+</style>
+
     <script>
         function calculateAge() {
            var birthdayInput = document.getElementById('birthday');
